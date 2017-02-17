574e1cba5ecf0a9106f784016f4e744466c7d05b
Jesús Ángel González Novez <jesusgonzaleznovez@gmail.com>
2017-02-17 12:05:23 +0100

    ES6 style functions, use strict too

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /* jshint esversion: 6 */
-
+'use strict';
 
 /**
  * D'hondt
@@ -7,30 +7,31 @@
  */
 (function () {
 
-    function compute(votes, names, options) {
+    const compute = (votes, names, options) => {
         let error = checkParams(votes,names,options);
         if(!error){
             return calculateSeats(votes, names, options.mandates, options.blankVotes, options.percentage);
         }else{
             console.log(error);
         }
-    }
-    function checkParams(votes,names,options){
+    };
+
+    const checkParams = (votes,names,options) => {
         if(!(votes.constructor.toString().indexOf("Array") > -1) || !(names.constructor.toString().indexOf("Array") > -1)) return new Error('Wrong params');
         if(votes.length !== names.length) return new Error('votes.length must to be equal to names.length');
         if(typeof options !== 'object') return new Error('Wrong options');
         return false;
-    }
+    };
 
-    function calculateTotalVotes(votes, blankVotes) {
+    const calculateTotalVotes = (votes, blankVotes) => {
         let total = parseInt(blankVotes);
         for (let i = 0, len = votes.length; i < len; ++i) {
             total = parseInt(votes[i]) + total;
         }
         return total;
-    }
+    };
 
-    function validateParties(numberOfParties, minNumberOfVotes, votes, names, validatedVotes, validatedNames) {
+    const validateParties = (numberOfParties, minNumberOfVotes, votes, names, validatedVotes, validatedNames) => {
         let numberOfPartiesValidated = 0;
         for (let i = 0; i < numberOfParties; ++i) {
             if (votes[i] >= minNumberOfVotes) {
@@ -40,9 +41,9 @@
             }
         }
         return numberOfPartiesValidated;
-    }
+    };
 
-    function newSeat(votos, esc, num_par) {
+    const newSeat = (votos, esc, num_par) => {
         let imax = 0, ct, max = 0;
         for (ct = 0; ct < num_par; ++ct) {
             if (max < (votos[ct] / (esc[ct] + 1))) {
@@ -51,24 +52,24 @@
             }
         }
         return imax;
-    }
+    };
 
-    function fillSeats(mandates, seats, validatedVotes, numberOfPartiesValidated) {
+    const fillSeats = (mandates, seats, validatedVotes, numberOfPartiesValidated) => {
         let table = [];
         for (let i = 0; i < mandates; ++i) {
             seats[newSeat(validatedVotes, seats, numberOfPartiesValidated)]++;
             table.push(seats.slice());
         }
         return table;
-    }
+    };
 
-    function fillPartiesResult(numberOfPartiesValidated, result, validatedNames, seats) {
+    const fillPartiesResult = (numberOfPartiesValidated, result, validatedNames, seats) => {
         for (let i = 0; i < numberOfPartiesValidated; ++i) {
             result.parties[validatedNames[i]] = seats[i];
         }
-    }
+    };
 
-    function calculateSeats(votes, names, mandates, blankVotes, percentage) {
+    const calculateSeats = (votes, names, mandates, blankVotes, percentage) => {
         let numberOfParties = votes.length,
             numberOfVotes = calculateTotalVotes(votes, blankVotes),
             minNumberOfVotes = Math.ceil(numberOfVotes * percentage / 100),
@@ -80,14 +81,16 @@
         let table = fillSeats(mandates, seats, validatedVotes, numberOfPartiesValidated);
         fillPartiesResult(numberOfPartiesValidated, result, validatedNames, seats);
         return result;
-    }
-    function fillResultVar(numberOfVotes,minNumberOfVotes){
+    };
+
+    const fillResultVar = (numberOfVotes,minNumberOfVotes) => {
         return {
             numberOfVotes: numberOfVotes,
                 minNumberOfVotes: minNumberOfVotes,
             parties: {}
         };
     }
+
     module.exports = {
         compute: compute
     };
